666f2763fc43e0fc9496bd680f33dcd1f6d0a52f
Nate Orona <60599423+StevenSeagal98@users.noreply.github.com>
2024-12-20 22:26:28 -0600

    Update About.vue

--- pages/About.vue
+++ pages/About.vue
@@ -8,14 +8,11 @@
                 I strive to always keep passion and a solution based mindset
                 at the forefront of my work. Anyone can throw together a website, 
                 my goal is to create the most streamlined, user friendly
-                experiences possible. <br/><br/>I'm 24 years old and currently 
-                employed full time by Virtus Media as a Digital Media 
-                Manager, which has broadened my skill set considerably and 
+                experiences possible. <br/><br/>I'm currently 
+                employed full time by Virtus Media as a Full Stack Web Developer, which has broadened my skill set considerably and 
                 afforded me an intimate look into the link between web 
                 development, product creation and marketing. In addition to 
-                this experience, I'm also currently pursuing a degree in Software 
-                Development and plan to continue working full time to build my 
-                experience while in school.
+                this experience, I have an AS in Software Development and am currently working towards a BS in Computer Science.
                 </p>
                 <div class="d-flex flex-column mt-4">
                     <h5>
@@ -23,45 +20,22 @@
                     </h5>
                     <p>These are some of the languages and tools I've used to build applications and websites in production and that I feel comfortable jumping into a new codebase or project with.</p>
                     <ul class="align-items-center d-flex tech">
-                        <li>HTML</li>
-                        <li>CSS</li>
                         <li>Javascript</li>
                         <li>PHP</li>
-                        <li>MySQL</li>
+                        <li>Java</li>
+                        <li>Tailwind</li>
+                        <li>Spring Boot</li>
+                        <li>Kotlin</li>
+                        <li>Postgresql</li>
                         <li>VueJS</li>
-                        <li>Nuxt</li>
-                        <li>Node</li>
                         <li>Express</li>
                         <li>WordPress</li>
                         <li>Azure</li>
-                        <li>HandlebarsJS</li>
-                        <li>Ghost</li>
-                        <li>MJML</li>
                         <li>Python</li>
-                        <li>Postgres</li>
-                        <li>Stripe API</li>
-                        <li>Sendgrid API</li>
+                        <li>MongoDB</li>
                         <li>Git / Github</li>
                     </ul>
                 </div>
-                <div class="d-flex flex-column mt-2">
-                    <h5>
-                        Technologies I've Dabbled With
-                    </h5>
-                    <p>These are some things I've played around with either creating applications for school or in small personal projects, and that I'd love a chance to dive deeper into.</p>
-                    <ul class="tech align-items-center d-flex">
-                        <li>React</li>
-                        <li>MySQL</li>
-                        <li>Django</li>
-                        <li>Flask</li>
-                        <li>Laravel</li>
-                        <li>Kotlin</li>
-                        <li>Discord API</li>
-                    </ul>
-                </div>
-            <div class="d-flex my-2 align-items-center flex-row justify-content-around">
-
-            </div>
             <h2 class="colored-text">Experience</h2>
             <div class="col-12 shadow round justify-content-start py-2">
                 <div class="d-flex flex-row flex-wrap">
@@ -69,16 +43,16 @@
                         <img src="https://i.imgur.com/iy1RRUt.jpg" alt="Virtus Media Logo" class="w-100 rounded-circle" />
                     </div>
                     <div class="d-flex flex-column justify-content-end pb-0 mb-0 text-light">
-                        <h5>Web Developer</h5>
+                        <h5>Full Stack Web Developer</h5>
                         <h6>Virtus Media</h6>
                         <h6>March 2021 - Current</h6>
                     </div>
                     <ul class="col-11 justify-self-end d-flex">
-                        <li>Refactored existing codebase of main PB Alerts web application with two backend engineers in NuxtJS and Node with Express to increase performance and greatly reduce code redundancy/clashing package dependencies.</li>
-                        <li>Built a web scraping application in Node and express to populate dynamic, hand coded email templates.</li>
-                        <li>Contributed to PHP WordPress themes for various media partners.</li>
+                        <li>Refactored legacy .NET web application in Vue, Node, and Postgresql to increase performance and eliminate security vulnerabilities.</li>
+                        <li>Reduced build times significantly by minimizing dependencies, modernizing tooling, and streamlining build pipelines with Github Actions and Azure.</li>
+                        <li>Designed and implemented a procedural email marketing and paid newsletter management service with NodeJS and Sendgrid.</li>
+                        <li>Contributed to PHP WordPress themes and built custom plugins for various media partners.</li>
                         <li>Created a variety of new products under the direction of the CEO and COO with various technologies.</li>
-                        <li>Improved community engagement by creating email system from the ground up with ExpressJS, Postgres, and the Sendgrid API.</li>
                     </ul>
                 </div>
             </div>
